d90de0449c1932fbe8d12c41e504d1fe15062285
Jing Yin Ong <ongjingyin@yahoo.com.sg>
2017-10-21 02:25:13 +0800

    init socket server

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 PubliclyTransparentServer-CS4236
+
+Run
+```
+npm install
+node index.js
+```
+
+To emulate a client, open a new tab in your command line, still residing in the same directory as this project, and run:
+```javascript
+var io = require('socket.io-client');
+var socket = io.connect('http://localhost:3000')
+socket.on('results', ({message}) => {console.log(message)});
+
+// To register as a player + send in bet of $5
+socket.emit('register', { bet: 5 });
+
+// To send in commit of value 10
+socket.emit('commit', { commit: 10 });
+
+// To send in guess of value 10
+socket.emit('guess', { guess: 10 });
+```
--- index.js
+++ index.js
@@ -1,7 +1,106 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const socketio = require('socket.io');
+const _ = require('lodash');
 
-app.set('port', (process.env.PORT || 5000));
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+const server = app.listen(PORT, function () {
+  console.log('Listening on port 3000')
+});
+
+let game = 123;	// rand int, changed when game ends
+let balance = 0;
+let transactions = {};
+
+const io = socketio.listen(server);
+io.on('connection', (socket) => {
+  console.log(`${socket.id} connected`);
+
+  // socket.emit('register', { bet: 5 });
+  socket.on('register', ({ bet }) => {
+  	if (bet && _.isSafeInteger(bet) && bet > 0) {
+    	console.log(`${socket.id} has been registered and placed bet of ${bet}`);
+      socket.join(game);
+      transactions[socket.id] = {};
+      balance += bet;
+  	}
+  });
+
+  // socket.emit('commit', { commit: 10 });
+  socket.on('commit', ({ commit }) => {
+  	if (!transactions[socket.id]) {
+  		return console.warn(`${socket.id} does not exist in transactions`);
+  	}
+  	console.log(`${socket.id} has submitted commit ${commit}`);
+  	// check that commit is valid data type
+  	transactions[socket.id].commit = commit;
+  });
+
+  // socket.emit('guess', { guess: 10 });
+  socket.on('guess', ({ guess }) => {
+		if (!transactions[socket.id]) {
+  		return console.warn(`${socket.id} does not exist in transactions`);
+  	}
+  	console.log(`${socket.id} has submitted guess ${guess}`);
+  	// check that guess is valid data type
+  	transactions[socket.id].guess = guess;
+  	revealSecret();
+  });
+});
+
+// to be called on timeout
+function revealSecret(){
+  let validTransactions = [];
+  let transactionsSize = _.keys(transactions).length;
+	let secret = 0;
+	_.forEach(transactions, (transaction, id) => {
+		let { guess, commit } = transaction;
+		if (guess) {
+      secret += transaction.guess % transactionsSize;
+      if (sha3(transaction.guess) === transaction.commit) {
+      	transaction.playerId = id;
+        validTransactions.push(transaction);
+			}
+		}
+	});
+
+	let winners = [];
+	_.forEach(validTransactions, (transaction) => {
+		if (_.isEqual(transaction.guess, secret)) {
+			winners.push(transaction.playerId);
+		}
+	});
+
+	if (_.isEmpty(winners)) {
+  	io.in(game).emit('results', { message: `No winners for this round, secret was ${secret}` });
+	}
+	else {
+  	io.in(game).emit('results', { message: `Winners are ${winners}, secret was ${secret}` });
+	}
+	// delete room
+	resetGame();
+}
+
+
+function resetGame() {
+	transactions = {};
+	let oldGame = game;
+	game = _.random(5000);
+	while (game === oldGame) {
+		game = _.random(5000);
+	}
+}
+
+function sha3(val) {
+	return val;	// dummy
+}
+
+
+
+
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,6 +112,3 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
